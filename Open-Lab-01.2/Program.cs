a49292e0d155f2b96d496f4982731003b81f6844
MatusDurana <matusdurana2mail.com>
2019-10-10 09:05:41 +0200

    Add project files.

---
+++ Open-Lab-01.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Open_Lab_01._2
+{
+    class Program
+    {
+        public static int Sum(int fnum, int snum)
+        {
+            return (fnum + snum);
+
+        }
+        static void Main(string[] args)
+        {
+            int fnum;
+            int snum;
+            int sum;
+
+            Console.WriteLine("Zadaj svoje prvé číslo");
+            fnum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadaj svoje druhé číslo");
+            snum = int.Parse(Console.ReadLine());
+
+            sum = Sum(fnum, snum);
+
+            Console.WriteLine($"Sum is: { sum }");
+        }
+    }
+} 
